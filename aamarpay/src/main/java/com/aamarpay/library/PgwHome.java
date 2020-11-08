24b21294d58c850463df4481184ba82134dae9d5
Rafat Hossain <rafathossain96@gmail.com>
2020-11-08 17:41:50 +0600

    initial commit

--- aamarpay/src/main/java/com/aamarpay/library/PgwHome.java
+++ aamarpay/src/main/java/com/aamarpay/library/PgwHome.java
@@ -2,8 +2,11 @@ package com.aamarpay.library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class PgwHome extends AppCompatActivity {
 
@@ -14,6 +17,23 @@ public class PgwHome extends AppCompatActivity {
 
         String url = getIntent().getStringExtra("URL");
         WebView webView = findViewById(R.id.pgw_web);
+        webView.setWebViewClient(new myWebClient());
         webView.loadUrl(url);
+    }
+
+    public static class myWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            view.loadUrl(url);
+            return true;
+
+        }
     }
 }

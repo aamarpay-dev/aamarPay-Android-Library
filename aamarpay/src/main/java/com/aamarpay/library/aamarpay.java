69452cf0bc1db94d0d3c1ae9c5639c46d58c456e
Rafat Hossain <rafathossain96@gmail.com>
2020-11-08 15:34:35 +0600

    Initial commit

---
+++ aamarpay/src/main/java/com/aamarpay/library/aamarpay.java
@@ -0,0 +1,14 @@
+package com.aamarpay.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aamarpay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aamarpay);
+    }
+}

8d598ddaeee429a24c3aed00bdc0c122b1c5f59b
Rafat Hossain <rafathossain96@gmail.com>
2020-11-09 13:46:22 +0600

    initial commit

--- aamarpay/src/main/java/com/aamarpay/library/AamarPay.java
+++ aamarpay/src/main/java/com/aamarpay/library/AamarPay.java
@@ -2,12 +2,30 @@ package com.aamarpay.library;
 
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+
+import java.util.Random;
 
 public class AamarPay {
+    public static final String DATA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static Random RANDOM = new Random();
+
     public static void init_pgw(Context context) {
         Intent intent = new Intent(context, PgwHome.class);
         intent.putExtra("URL", "https://google.com");
         context.startActivity(intent);
+    }
+
+    public static String generate_trx_id() {
+        return randomString() + System.currentTimeMillis();
+    }
+
+    private static String randomString() {
+        StringBuilder sb = new StringBuilder(3);
+
+        for (int i = 0; i < 3; i++) {
+            sb.append(DATA.charAt(RANDOM.nextInt(DATA.length())));
+        }
+
+        return sb.toString();
     }
 }
--- app/src/main/java/com/aamarpay/payment/MainActivity.java
+++ app/src/main/java/com/aamarpay/payment/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aamarpay.payment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_LONG).show();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 }
